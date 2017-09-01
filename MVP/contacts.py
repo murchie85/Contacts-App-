26afef46badd97d1fb2d71fce1a26fb9e77505f7
Adam <murchie85@gmail.com>
2017-09-01 21:44:46 +0100

    f

--- MVP/contacts.py
+++ MVP/contacts.py
@@ -1,5 +1,6 @@
 import sqlite3
 import random
+import string
 
 '''
 Please write a small web application that models an address book. Your solution should present a simple user--interface
@@ -19,8 +20,6 @@ EMAIL
 MOBILE
 '''
 
-
-
 #define DB connection
 conn = sqlite3.connect('contactsdb.db')
 c = conn.cursor()
@@ -36,7 +35,35 @@ def input_test():
 	conn.commit()
 	c.close()
 	conn.close()
+# for populating random name
+def random_char(y):
+       return ''.join(random.choice(string.ascii_letters) for x in range(y))
+# make a random tel number
+def random_tel(y):
+	for x in range(y):
+		num = num + random.randrange(0,9)
+		return num
+
+# allows for inserting based upon parameratization 
+def dynamic_data_entry():
+	value = random.randrange(0,15)
+	name = random_char(value)
+	org = random_char(value)
+	tel = random_tel(value)
+	email = random_char(value) + '@' + random_char(value) + '.com'
+	mob = random_tel(value)
+	c.execute("INSERT INTO allContacts (name, org, tel, email, mob) VALUES (?, ?, ?, ?, ?)",
+	(name, org, tel, email, mob)) #MYSQL will use %s instead of ?
+	conn.commit() # no need to close after commit, especially if doing multiple inserts
+
 
 
 create_table()
-input_test()
+#input_test()
+
+# add 10 rows randomised 
+for i in range(10):
+	dynamic_data_entry()
+
+c.close()
+conn.close()
